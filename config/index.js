47860337018ee1a01bb45fc14a6c15853fd9f8e5
dullsword <525649253@qq.com>
2022-05-07 17:45:30 +0800

    rebuild

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/hitokoto-api-web/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
